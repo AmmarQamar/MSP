798ae2b29123cee93e1c4ffc82c981d1dd49f308
AmmarQamar <rajaammarqamar@gmail.com>
2023-05-21 04:10:03 +0500

    mongondb connect

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,40 @@
-// import express from "express" // for this package jason type="module"
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const cors = require("cors");
+const express = require("express");
+const { json, urlencoded } = require("express");
 
-const PORT = 3000
+const app = express();
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000
+mongoose.connect('mongodb://127.0.0.1:27017/MSP', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log("MongoDB connected");
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/MSP');
+// Routes
+app.get("/", (req, res) => {
+    rs.send("My APi")
+})
 
+app.post("/sellerregister", (req, res) => {
+    res.send("My Api Rgister")
+})
 
-i
+app.post("/sellerlogin", (req, res) => {
+    res.send("Seller Login")
+})
+app.listen(PORT, () => {
+    debugger
+    console.log(`Run on PORT ${PORT} `);
+});
