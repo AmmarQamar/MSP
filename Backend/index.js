e20c139037d34b9f6513ea1524b42c1370f4ee65
AmmarQamar <103876619+AmmarQamar@users.noreply.github.com>
2023-05-23 20:13:48 +0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 const PORT = process.env.PORT || 9002
+//Database Connect
 mongoose.connect('mongodb://127.0.0.1:27017/MSP', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -21,7 +22,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/MSP', {
         console.error("Error connecting to MongoDB:", error);
     });
 
-// Schema
+// Seller Schema
 const sellerSchema = new mongoose.Schema({
     fullname: String,
     email: String,
@@ -29,9 +30,28 @@ const sellerSchema = new mongoose.Schema({
     address: String,
     password: String
 })
+// Add Mart Schema
+const martSchema = new mongoose.Schema({
+    title: String,
+    owner: String,
+    address: String,
+    contact: String,
+    location: {
+        lat: Number,
+        lng: Number
+    }
 
-//Model
+})
+
+//      Models    
+//Seller Model
 const Seller = new mongoose.model("Seller", sellerSchema)
+//Mart Model
+const Mart = new mongoose.model("Mart", martSchema)
+
+
+
+
 
 // Routes
 app.get("/", (req, res) => {
@@ -39,35 +59,7 @@ app.get("/", (req, res) => {
 })
 
 //                  POST  Routes
-// Seller Register
-app.post("/register", (req, res) => {
-    const { fullname, email, phoneno, address, password } = req.body;
-    Seller.findOne({ email: email })
-        .then(seller => {
-            if (seller) {
-                res.send({ message: `${seller.email} already registered` })
-            }
-            else {
-                const seller = new Seller({
-                    fullname,
-                    email,
-                    phoneno,
-                    address,
-                    password
-                })
 
-                seller.save()
-                    .then(() => {
-                        console.log("Seller saved successfully");
-                        res.status(200).json({ message: "Seller registered successfully" });
-                    })
-                    .catch((error) => {
-                        console.error("Error saving seller:", error);
-                        res.status(500).json({ message: "Error saving seller" });
-                    });
-            }
-        })
-})
 // Seller Login
 app.post("/login", (req, res) => {
     const { email, password } = req.body;
@@ -94,6 +86,67 @@ app.get('/logout', (req, res) => {
     res.clearCookie('jwtoken', { path: '/' })
     res.status(200).send("Logout Successfull")
 })
+
+// Seller Register
+app.post("/register", (req, res) => {
+    const { fullname, email, phoneno, address, password } = req.body;
+    Seller.findOne({ email: email })
+        .then(seller => {
+            if (seller) {
+                res.send({ message: `${seller.email} already registered` })
+            }
+            else {
+                const seller = new Seller({
+                    fullname,
+                    email,
+                    phoneno,
+                    address,
+                    password
+                })
+                seller.save()
+                    .then(() => {
+                        console.log("Seller saved successfully");
+                        res.status(200).json({ message: "Seller registered successfully" });
+                    })
+                    .catch((error) => {
+                        console.error("Error saving seller:", error);
+                        res.status(500).json({ message: "Error saving seller" });
+                    });
+            }
+        })
+})
+
+// Seller Add Mart
+app.post('/addmart', (req, res) => {
+    const { title, owner, address, contact, location } = req.body
+    debugger
+    const { lat, lng } = location;
+
+    const mart = new Mart({
+        title,
+        owner,
+        address,
+        contact,
+        location: {
+            lat,
+            lng
+        }
+    })
+    mart.save()
+        .then(() => {
+            console.log("Mart Added Successfully")
+            res.status(200).json({ message: "Mart Successfully Registered " })
+        })
+        .catch(() => {
+            res.json("Mard Not Registered in database")
+        })
+
+})
+
+
+
+
+
 app.listen(PORT, () => {
     debugger
     console.log(`Run on PORT ${PORT} `);
